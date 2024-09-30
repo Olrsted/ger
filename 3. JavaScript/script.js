9d7501e69859a4da3d053f252e72f5905087b2c2
Omar Qayum <omar.qayum@tdsb.on.ca>
2024-09-30 11:12:00 -0400

    Added functions and array code to JS

--- 3. JavaScript/script.js
+++ 3. JavaScript/script.js
@@ -59,3 +59,87 @@ x %= 5;
 x = x + 1; // don't do this!
 x += 1; // better but not good enough
 x++; // Perfect!
+
+// BOOLEAN OPERATORS
+// 1. Boolean operators should only be used in expressions that should result in true or false
+// 2. They can work with just about any type, if used correctly
+// 3. It is important to note that JavaScript has "truthy" and "falsy" values
+
+let a = 6;
+let b = 10;
+let c = true;
+let d = false;
+a > b; // checks if a is greater than b
+a < b; // checks if a is less than b
+a >= b; // checks if a is greater than or equal to b
+a <= b; // checks if a is less then or equal to b
+a == b; // checks if a is equal to b; if types are different JavaScript will coerce the variables
+a === b; // checks if a is equal to be and of the same type; no coercion
+c & d; // checks c AND d
+c && d; // checks c AND d, but uses short-circuit logic (i.e. if c is false, d is not checked)
+c | d; // checks c OR d
+c || d; // checks c OR d, but uses short-circuit logic (i.e. if c is true, d is not checked)
+
+// FUNCTIONS
+// 1. Functions are blocks of code that are executed upon invocation
+// 2. They can take parameters/arguments, if necessary
+// 3. They can only return a single value, if necessary
+// 4. They can be written using the "function" keyword or with arrow syntax "=>"
+
+// A Pythagorean Theorem function that takes two arguments and returns a value
+function pythagoreanTheorem1(a, b) {
+  return Math.sqrt(a * a + b * b);
+}
+
+// The same function as above using arrow syntax
+const pythagoreanTheorem2 = (a, b) => {
+  return Math.sqrt(a * a + b * b);
+}
+
+// Executing/calling the above functions and storing their results in variables
+const c1 = pythagoreanTheorem1(2, 1);
+const c2 = pythagoreanTheorem2(2, 1);
+
+const ccc = (a, b, c) => {
+    console.log(c(a, b));
+}
+
+ccc(2, 1, pythagoreanTheorem2);
+
+// ARRAYS
+// 1. Arrays can contain a set values
+// 2. Normally they contain values of the same type, but they can be mixed
+// 3. The values can be unique or duplicates
+// 4. Each value is indexed starting from 0 and ending at length - 1
+// 5. They can use the spread operator "..." and be destructured
+// 6. They have useful built-in functions like map, filter and reduce
+
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // an array of integers
+const array2 = [2, 4.3, "6", true, [1, 2, 3], {}]; // an array of mixed types
+const array3 = [...array1, ...array2]; // spreads the contents of array1 and array2 into a new array
+let [first, second, ...rest] = array1; // destructures the 1st, 2nd and remaining values into variables called first, second and rest, respectively
+const length = array1.length; // determines the length (size) of array1
+array1[0]; // gets the value at index 0 (first) from array1
+array1[9]; // gets the value at index 9 (last) from array1
+array1[0] = 10; // changes the value at index 0 of array1 to 10
+array1[9] = 10; // changes the value at index 9 of array1 to 10
+
+// the map function will loop through array1 and multiply all its values by 10; it returns a new array
+const map = array1.map((value) => {
+  return 10 * value;
+});
+
+// the filter function will loop through array1 and only keep values that are even; it returns a new array
+const filter = array1.filter((value) => {
+  return value % 2 === 0;
+});
+
+// the reduce function will loop through array1 and sum all the values with the sum starting at 10; it returns a new array
+const reduce = array1.reduce((prev, cur) => {
+  return prev + cur;
+}, 10);
+
+array1.pop(); // removes the last value in the array and returns it
+array1.push(10); // adds 10 to the end of the array
+array1.shift(); // removes the first value in the array and returns it
+array1.unshift(10); // adds 10 to the front of the array
